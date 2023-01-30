32cc3a0cfb157937249733ff4e6022434979c336
Laboratorio Asu <laboratorioasu@unida.edu.py>
2023-01-30 16:05:20 -0300

    clase final

--- login.py
+++ login.py
@@ -1,6 +1,7 @@
 """
 Ejemplo:
 Url: http://localhost:5000/login
+cURL -X PUT -d '{"user": "derlis", "passw": "123"}' -H "Content-Type: application/json" http://localhost:5001/login
 Datos de entrada:
     
 {
@@ -17,16 +18,39 @@ Salida:
 
 
 """
+from ast import Str
 from flask import Blueprint, request, jsonify
 login = Blueprint('login', __name__)
+
 @login.route('/login', methods=['POST'])
 def llamarServicioSet():
-    global user,passw
+    global user,password
     ##try:
-    user =request.json['pepe']
-    passw =request.json['passw']
+    #pediremos como dato de entrada user - pas
+    user=request.json['user']
+    password =request.json['password']
+    inicializarVariables(user,password)
+    
+     
+    salida = {'codRes': codRes, 'menRes': menRes, 'usuario':user,'accion':accion}
+    return jsonify(salida)
 
-    print("Username",user)
-    print("Password",passw)
+def inicializarVariables(user,password):
+    global codRes, menRes,accion
+    userLocal="dcaballero"
+    passLocal="unida123"
+    codRes = 'SIN_ERROR'
+    menRes = 'OK'
+    try:
+        print("Verificar login")
+        if(str(password)==str(passLocal) and str(user)==str(userLocal)):
+            print("Usuario y contraseña OK")
+            accion="Succes"
+        else:
+            print("Usuario o contraseña incorrecta")
+            accion="Usuario o contraseña incorrecta"
 
-    #inicializarVariables(categoryId)
+    except Exception as e:
+        print("ERROR",str(e))
+        codRes='ERROR'
+        menRes='Msg'+str(e)
